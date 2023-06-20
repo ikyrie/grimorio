af488f6d840546ab7c1ca6c1a03dfd450275f469
ikyrie <mah.c5r@gmail.com>
2023-06-20 18:02:07 -0300

    Removing useless import

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:grimorio/primary_button.dart';
-import 'package:grimorio/secondary_button.dart';
 import 'package:grimorio/theme.dart';
 
 class Dashboard extends StatelessWidget {
