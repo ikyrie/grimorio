06dedbca90ce3f46ab505fddd6a40444ab68d369
ikyrie <mah.c5r@gmail.com>
2023-06-27 13:02:49 -0300

    Separating empty home in another widget

--- lib/home.dart
+++ lib/home.dart
@@ -16,32 +16,41 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return SafeArea(child: Container(
       decoration: AppBackgroundProperties.boxDecoration,
-      child: Scaffold(
+      child: const Scaffold(
         backgroundColor: Colors.transparent,
         body: Center(
           child: SingleChildScrollView(
-            child: Column(children: <Widget>[
-              const Padding(
-                padding: EdgeInsets.only(bottom: 32.0),
-                child: DisplayText("Grimório"),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 40.0),
-                child: SvgPicture.asset("assets/images/grimorio_empty.svg"),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
-                child: Text("Seu Grimório está vazio!", style: TextStyle(fontFamily: "Bigelow Rules", fontSize: 36, color: AppColors.lightPink),),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(bottom: 40.0),
-                child: Text("Vamos aprender algo novo?", style: TextStyle(fontWeight: FontWeight.w500),),
-              ),
-              FloatingButton(onTap: (){}),
-            ]),
+            child: _EmptyHome(),
           ),
         ),
       ),
     ));
+  }
+}
+
+class _EmptyHome extends StatelessWidget {
+  const _EmptyHome();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      const Padding(
+        padding: EdgeInsets.only(bottom: 32.0),
+        child: DisplayText("Grimório"),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 40.0),
+        child: SvgPicture.asset("assets/images/grimorio_empty.svg"),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: Text("Seu Grimório está vazio!", style: TextStyle(fontFamily: "Bigelow Rules", fontSize: 36, color: AppColors.lightPink),),
+      ),
+      const Padding(
+        padding: EdgeInsets.only(bottom: 40.0),
+        child: Text("Vamos aprender algo novo?", style: TextStyle(fontWeight: FontWeight.w500),),
+      ),
+      FloatingButton(onTap: (){}),
+    ]);
   }
 }
